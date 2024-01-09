eac7ea56195af2a75aa0083a9f310516aba49f05
francis <willenerfrancis0@gmail.com>
2024-01-09 11:29:08 -0300

    Settings to deploy

--- src/middleware.js
+++ src/middleware.js
@@ -36,7 +36,7 @@ export default auth((req) => {
 });
 
 export const config = {
-  runtime: "edge", // for Edge API Routes only
+  runtime: "experimental-edge", // for Edge API Routes only
   unstable_allowDynamic: [
     "/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
   ],
