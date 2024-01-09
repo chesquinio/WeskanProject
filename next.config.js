8f9443a6967ad8401c820075372985acad04a178
francis <willenerfrancis0@gmail.com>
2024-01-09 12:02:27 -0300

    Settings to deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Excluye el archivo mongoose.umd.js del lado del cliente
+      config.resolve.alias[
+        "./node_modules/mongoose/dist/browser.umd.js"
+      ] = false;
+    }
     config.resolve.fallback = {
       "mongodb-client-encryption": false,
       aws4: false,
