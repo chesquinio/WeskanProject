8386897c2b449a436e0753bf0515e929e08fc42a
francis <willenerfrancis0@gmail.com>
2024-01-31 15:07:24 -0300

    Modified google login button

--- src/components/auth/login/social.jsx
+++ src/components/auth/login/social.jsx
@@ -2,7 +2,6 @@
 
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebook } from "react-icons/fa";
 import { DEFAULT_LOGIN_REDIRECT } from "../../../../routes";
 
 export default function Social() {
@@ -14,18 +13,13 @@ export default function Social() {
     });
   };
   return (
-    <>
-      <h5 className="text-sm text-gray-600 font-medium mb-2">
-        Inicia sesión con Google
-      </h5>
-      <div className="flex items-center w-full">
-        <button
-          onClick={(event) => onClick(event, "google")}
-          className="flex justify-center w-full py-2 px-4 rounded bg-transparent border border-gray-300 hover:border-pink-400"
-        >
-          <FcGoogle className="w-5 h-5" />
-        </button>
-      </div>
-    </>
+    <div className="flex items-center w-full">
+      <button
+        onClick={(event) => onClick(event, "google")}
+        className="flex justify-center items-center gap-3 w-full py-2 px-4 text-gray-600 font-medium rounded bg-transparent border border-gray-400 hover:bg-gray-100 hover:shadow-sm hover:shadow-gray-600 transition-all"
+      >
+        <FcGoogle className="w-5 h-5" /> Google
+      </button>
+    </div>
   );
 }
