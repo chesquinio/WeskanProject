5c2b90ce008afc29f58e159e6f720d7f3657bae8
francis <willenerfrancis0@gmail.com>
2024-01-31 11:47:04 -0300

    Facebook login removed

--- src/components/auth/login/social.jsx
+++ src/components/auth/login/social.jsx
@@ -14,19 +14,18 @@ export default function Social() {
     });
   };
   return (
-    <div className="flex items-center w-full gap-x-2">
-      <button
-        onClick={(event) => onClick(event, "google")}
-        className="flex justify-center w-full py-2 px-4 rounded bg-transparent border border-gray-300 hover:border-pink-400"
-      >
-        <FcGoogle className="w-5 h-5" />
-      </button>
-      <button
-        onClick={(event) => onClick(event, "facebook")}
-        className="flex justify-center w-full py-2 px-4 rounded bg-transparent border border-gray-300 hover:border-pink-400"
-      >
-        <FaFacebook className="w-5 h-5" />
-      </button>
-    </div>
+    <>
+      <h5 className="text-sm text-gray-600 font-medium mb-2">
+        Inicia sesión con Google
+      </h5>
+      <div className="flex items-center w-full">
+        <button
+          onClick={(event) => onClick(event, "google")}
+          className="flex justify-center w-full py-2 px-4 rounded bg-transparent border border-gray-300 hover:border-pink-400"
+        >
+          <FcGoogle className="w-5 h-5" />
+        </button>
+      </div>
+    </>
   );
 }
