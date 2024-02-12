3047a9f39da851526085273881bda5585ec8e020
francis <willenerfrancis0@gmail.com>
2024-02-12 12:32:02 -0300

    Updating file buttons styles

--- src/components/file-button.jsx
+++ src/components/file-button.jsx
@@ -66,10 +66,10 @@ export default async function FileButtons() {
               src={file ? `/${file.name}.webp` : "/weskan-logo.webp"}
               className="w-full h-full rounded object-cover object-center transition-transform transform scale-100 lg:group-hover:scale-105"
             />
-            <div className="absolute inset-0 pb-8 pl-5 pr-16 flex flex-col items-start justify-end z-5 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity bg-gray-500 bg-opacity-50 text-white">
+            <div className="absolute inset-0 pb-8 pl-5 pr-16 flex flex-col items-start justify-end z-5 opacity-100 lg:group-hover:bg-opacity-60 transition-opacity bg-gray-500 bg-opacity-50 text-white">
               <h3
                 style={{ textShadow: "0px 4px 19px rgba(0,0,0,0.6)" }}
-                className="text-xl md:text-3xl"
+                className="text-xl lg:text-3xl lg:group-hover:ml-3 transition-all"
               >
                 {file ? formatName(file.name) : "Nombre no disponible"}
               </h3>
