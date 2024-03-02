663e97c1036891d56c85bacabb8cb945b4fde696
francis <willenerfrancis0@gmail.com>
2024-03-02 17:04:06 -0300

    New style

--- src/app/(main)/unirse/page.jsx
+++ src/app/(main)/unirse/page.jsx
@@ -1,9 +1,13 @@
 import UploadCurriculum from "@/components/main/upload-curriculum";
 
+export const metadata = {
+  title: `Unirse`,
+};
+
 export default function JoinPage() {
   return (
     <main className="m-5">
-      <section className="w-full md:max-w-5xl mx-auto min-h-[calc(100dvh-120px)]">
+      <section className="w-full md:max-w-[900px] mx-auto min-h-[calc(100dvh-120px)]">
         <h2 className="text-2xl font-bold text-gray-900 md:text-4xl xl:text-5xl md:leading-normal mt-10 mb-20">
           Tu curriculum es de nuestro interés.
         </h2>
