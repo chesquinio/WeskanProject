9257b986f7d94f6fd7330880ff969446fb8423f3
francis <willenerfrancis0@gmail.com>
2024-03-02 17:03:41 -0300

    Modify catalogue content

--- src/app/(main)/catalogo/page.jsx
+++ src/app/(main)/catalogo/page.jsx
@@ -1,18 +1,8 @@
-import FileButtons from "@/components/file-button";
-import ListRequestForm from "@/components/main/list-request-form";
-import { FileButtonsSkeleton } from "@/components/skeletons";
-import { currentUser } from "@/lib/auth";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { Suspense } from "react";
-
 export const metadata = {
   title: `Catalogo`,
 };
 
 export default async function CataloguePage() {
-  const user = await currentUser();
-
   return (
     <main className="m-5">
       <section className="flex grow flex-col gap-4 md:flex-row min-h-[calc(100vh-120px)]">
@@ -47,43 +37,6 @@ export default async function CataloguePage() {
           />
         </div>
       </section>
-
-      {user ? (
-        <>
-          {user?.validated ? (
-            <section className="mx-auto max-w-[1200px] py-24 sm:py-32">
-              <h2 className="text-4xl font-bold text-center text-gray-800 mb-12">
-                Conoce nuestra listas de precios.
-              </h2>
-              <Suspense fallback={<FileButtonsSkeleton />}>
-                <FileButtons />
-              </Suspense>
-            </section>
-          ) : (
-            <section className="mx-auto max-w-[900px] py-24 sm:py-32">
-              <h3 className="text-2xl font-bold text-center text-gray-800 mb-12">
-                Solicitar acceso a listas de precios.
-              </h3>
-              <ListRequestForm />
-            </section>
-          )}
-        </>
-      ) : (
-        <section className="mx-auto max-w-[900px] py-24 sm:py-32">
-          <h3 className="text-2xl font-bold text-center text-gray-800 mb-12">
-            ¿Quiéres conocer nuestras listas de precios?
-          </h3>
-          <div className="flex justify-center items-center">
-            <Link
-              href="/iniciar-sesion"
-              className="md:text-lg xl:text-xl font-medium leading-6 text-pink-400 hover:text-pink-500 flex items-center gap-3 hover:gap-5 transition-all"
-            >
-              Iniciar Sesión
-              <ArrowRightIcon className="w-5 h-5" />
-            </Link>
-          </div>
-        </section>
-      )}
     </main>
   );
 }
