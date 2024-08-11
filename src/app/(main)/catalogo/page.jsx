824d542990dc8298284d4de339dac90b73822ced
Francis Willener <willenerfrancis0@gmail.com>
2024-08-11 17:41:31 -0300

    New style catalogue section

--- src/app/(main)/catalogo/page.jsx
+++ src/app/(main)/catalogo/page.jsx
@@ -7,42 +7,10 @@ export const metadata = {
 export default async function CataloguePage() {
   return (
     <main className="m-5">
-      <section className="flex grow flex-col gap-4 md:flex-row min-h-[calc(100vh-120px)]">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-200 px-6 py-10 md:w-2/5 md:px-20">
-          <h1 className="text-2xl font-bold text-gray-900 md:text-4xl xl:text-5xl md:leading-normal">
-            Productos para todos los motores.
-          </h1>
-          <p
-            className={`text-cl text-gray-800 md:text-2xl xl:text-3xl md:leading-normal`}
-          >
-            Aquí podras encontrar un listado con todos nuestros
-            <b className="text-pink-400"> productos</b> en un
-            <b className="text-pink-400"> solo lugar</b>, simplificando el
-            proceso de pedidos y solicitudes.
-          </p>
-          <p
-            className={`text-lg text-gray-800 md:text-xl xl:text-2xl md:leading-normal`}
-          >
-            Contamos con <strong>cientos de productos</strong> en fabricación.
-          </p>
-        </div>
-        <div className="flex items-center justify-center opacity-60 p-6 md:w-3/5 md:px-28 md:py-12">
-          <img
-            src="/catalogue-image.webp"
-            className="hidden md:block rounded-lg"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <img
-            src="/catalogue-image-mobile.webp"
-            className="block md:hidden rounded-lg"
-            alt="Screenshots of the dashboard project showing mobile version"
-          />
-        </div>
-      </section>
-      <section className="max-w-7xl mx-auto py-24 sm:py-32">
-        <h3 className="font-bold text-4xl text-gray-900">
+      <section className="max-w-7xl mx-auto mb-32">
+        <h2 className="text-2xl font-bold text-gray-900 md:text-4xl xl:text-5xl md:leading-normal mt-10 mb-20">
           Nuestros catálogos.
-        </h3>
+        </h2>
         <CatalogueButtons />
       </section>
     </main>
--- src/components/catalogue-buttons.jsx
+++ src/components/catalogue-buttons.jsx
@@ -1,45 +1,63 @@
-import { getLastsCatalogues } from "@/lib/data"
-import { DeleteButton } from "./admin/delete-button-catalogue"
+import { getLastsCatalogues } from "@/lib/data";
+import { DeleteButton } from "./admin/delete-button-catalogue";
 
 export default async function CatalogueButtons({ admin }) {
-  const catalogues = await getLastsCatalogues()
+  const catalogues = await getLastsCatalogues();
 
   return (
-    <div className="flex flex-col">
-          {catalogues.map((catalog) => (
-            <div
-              key={catalog.id}
-              className="relative w-full h-96 md:h-[340px] bg-pink-300 rounded-lg my-10 md:my-16"
-            >
-              <div className="w-full h-full">
-                <img
-                  src="/catalog-main-image.webp"
-                  alt={catalog.name}
-                  className="w-full h-full object-cover rounded-lg"
-                />
-              </div>
-              <div className="absolute flex flex-col gap-3 justify-between rounded-br-lg md:rounded-br-none md:rounded-tl-lg rounded-bl-lg bottom-0 md:top-0 left-0 text-white bg-black bg-opacity-75 backdrop-blur p-5 w-full md:w-[360px] h-auto md:h-full">
-                <div className="flex flex-col gap-1">
-                  <h4 className="font-semibold text-xl">{catalog.name}</h4>
-                  <p className="text-sm text-gray-300">
-                    {catalog.description}
-                  </p>
-                </div>
-                <div className="w-full flex flex-row gap-3">
-                  <a
-                    href={catalog.file}
-                    target="_blank"
-                    className="w-full flex justify-center items-center px-4 py-2.5 rounded-lg hover:bg-white hover:bg-opacity-15 border-gray-500 border font-semibold text-sm"
-                  >
-                    Ver catálogo
-                  </a>
-                  {admin && (
-                    <DeleteButton id={catalog.id} />
-                  )}
-                </div>
-              </div>
+    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      {catalogues.map((catalog) => (
+        // <div
+        //   key={catalog.id}
+        //   className="relative w-full h-96 md:h-[340px] bg-pink-300 rounded-lg my-10 md:my-16"
+        // >
+        //   <div className="w-full h-full">
+        //     <img
+        //       src="/catalog-main-image.webp"
+        //       alt={catalog.name}
+        //       className="w-full h-full object-cover rounded-lg"
+        //     />
+        //   </div>
+        //   <div className="absolute flex flex-col gap-3 justify-between rounded-br-lg md:rounded-br-none md:rounded-tl-lg rounded-bl-lg bottom-0 md:top-0 left-0 text-white bg-black bg-opacity-75 backdrop-blur p-5 w-full md:w-[360px] h-auto md:h-full">
+        //     <div className="flex flex-col gap-1">
+        //       <h4 className="font-semibold text-xl">{catalog.name}</h4>
+        //       <p className="text-sm text-gray-300">{catalog.description}</p>
+        //     </div>
+        //     <div className="w-full flex flex-row gap-3">
+        //       <a
+        //         href={catalog.file}
+        //         target="_blank"
+        //         className="w-full flex justify-center items-center px-4 py-2.5 rounded-lg hover:bg-white hover:bg-opacity-15 border-gray-500 border font-semibold text-sm"
+        //       >
+        //         Ver catálogo
+        //       </a>
+        //       {admin && <DeleteButton id={catalog.id} />}
+        //     </div>
+        //   </div>
+        // </div>
+        <div key={catalog.id} className="group relative">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img
+              alt={catalog.name}
+              src="/catalog-main-image.webp"
+              className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+          </div>
+          <div className="mt-4 flex justify-between">
+            <div>
+              <h3 className="text-md font-medium text-gray-700">
+                <a target="_blanck" href={catalog.file}>
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  {catalog.name}
+                </a>
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">
+                {catalog.description}
+              </p>
             </div>
-          ))}
+          </div>
         </div>
-  )
+      ))}
+    </div>
+  );
 }
